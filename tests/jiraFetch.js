902bc8dd985849c69d22e86998e00007f5bcb961
Karel Piwko <kpiwko@redhat.com>
2016-11-08 18:09:58 +0100

    Fixed edge cases fetch handling

--- tests/jiraFetch.js
+++ tests/jiraFetch.js
@@ -4,13 +4,34 @@ const test = require('blue-tape')
 const jiraClient = require('../lib/jira/client')
 const jiraFetch = require('../lib/jira/fetch')
 
-test('Fetch public AGPUSH project', (t) => {
+const jira = jiraClient('https://issues.jboss.org', '', '')
 
-  const jira = jiraClient('https://issues.jboss.org', '', '')
-
+test('Fetch public AGPUSH project', t => {
   return jiraFetch(jira, 'project = AGPUSH', {fields: ['Summary']})
     .then(issues => {
-      t.ok(issues, `JIRA issues have been fetched`)
+      t.ok(issues, 'AGPUSH issues have been fetched')
+    })
+    .catch(err => {
+      t.fail(err, 'Failed fetching issues')
+    })
+})
+
+test('Fetch single issue', t => {
+  return jiraFetch(jira, 'key = AGPUSH-1', {fields: ['Summary'], maxResults: 1})
+    .then(issues => {
+      t.ok(issues, 'AGPUSH-1 issue has been fetched')
+      t.equal(issues.length, 1, 'There is just one jira fetched')
+    })
+    .catch(err => {
+      t.fail(err, 'Failed fetching issues')
+    })
+})
+
+test('Fetch issues via multiple calls and limit number of issues', t => {
+  return jiraFetch(jira, 'project = AGPUSH', {fields: ['Summary'], maxResults: 1200})
+    .then(issues => {
+      t.ok(issues, 'AGPUSH issues has been fetched')
+      t.equal(issues.length, 1200, 'There has been 1200 issues fetched')
     })
     .catch(err => {
       t.fail(err, 'Failed fetching issues')

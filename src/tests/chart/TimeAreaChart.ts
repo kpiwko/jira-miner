16053ef514a692b3fb3e393b23ecc6cfafd7b113
Karel Piwko <kpiwko@redhat.com>
2019-09-24 16:20:29 +0200

    feat: added trendline option to AreaChart

--- src/tests/chart/TimeAreaChart.ts
+++ src/tests/chart/TimeAreaChart.ts
@@ -35,8 +35,49 @@ test('Render simple stacked area chart with links', async t => {
 
   const $ = cheerio.load(chartRender.svg, { xmlMode: true })
   t.is($('a').length, 2, 'There are two links in the svg image')
-
   t.is(chartRender.json.labels.length, 2, 'Chart data labels are stored in json representation')
+})
+
+test('Render simple chart with trendline', async t => {
+
+  const chart = new TimeAreaChart({
+    name: 'Test Links',
+    axisNames: ['X', 'Y', 'Y2'],
+    labels: ['Open', 'In Progress'],
+    trendLabels: ['Done']
+  })
+
+  const chartRender = await chart.render(dataFixture().map(d => {
+    if (d['In Progress'] > 5) {
+      d.link = 'https://foobar.com'
+    }
+    return d
+  }))
+
+  const $ = cheerio.load(chartRender.svg, { xmlMode: true })
+  t.is($('a').length, 2, 'There are two links in the svg image')
+  t.is($('.trendline').length, 1, 'There is one trendline in the svg image')
+})
+
+test('Render simple chart with two trendlines', async t => {
+
+  const chart = new TimeAreaChart({
+    name: 'Test Links',
+    axisNames: ['X', 'Y', 'Y2'],
+    labels: ['Open', 'In Progress'],
+    trendLabels: ['Done', 'Abandoned']
+  })
+
+  const chartRender = await chart.render(dataFixture().map(d => {
+    if (d['In Progress'] > 5) {
+      d.link = 'https://foobar.com'
+    }
+    return d
+  }))
+
+  const $ = cheerio.load(chartRender.svg, { xmlMode: true })
+  t.is($('a').length, 2, 'There are two links in the svg image')
+  t.is($('.trendline').length, 2, 'There are two trendlines in the svg image')
 })
 
 function dataFixture(): TimeAreaChartItem[] {
@@ -44,13 +85,25 @@ function dataFixture(): TimeAreaChartItem[] {
     date: '2019-06-09',
     'Open': 12,
     'In Progress': 7,
+    'Done': 6,
+    'Abandoned': 4
   }, {
     date: '2019-06-10',
     'Open': 12,
-    'In Progress': 4
+    'In Progress': 4,
+    'Done': 6,
+    'Abandoned': 5
   }, {
     date: '2019-06-11',
     'Open': 3,
     'In Progress': 6,
+    'Done': 9,
+    'Abandoned': 7
+  }, {
+    date: '2019-06-12',
+    'Open': 3,
+    'In Progress': 3,
+    'Done': 7,
+    'Abandoned': 9
   }]
 }
